28b11ee1f4d0d540e5501953dcfa19c386992f8c
gaussell <DEMPSEY.STRRXT@gmail.com>
2025-12-09 13:05:06 +0100

    Hero (broken)

--- Pura/Program.cs
+++ Pura/Program.cs
@@ -63,6 +63,7 @@ namespace Pura
             app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
             app.UseHttpsRedirection();
             app.UseAntiforgery();
+            app.UseStaticFiles();
             app.MapStaticAssets();
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
